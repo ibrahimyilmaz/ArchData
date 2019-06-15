11099b304070c36e3775ebf8abd8ed9c7a107b53
İbrahim Yilmaz <iylmaz@ymail.com>
2019-06-15 13:06:30 +0200

    nonNullObserver method is added.

--- src/main/java/me/ibrahimyilmaz/arch_data/LiveDataExtensions.kt
+++ src/main/java/me/ibrahimyilmaz/arch_data/LiveDataExtensions.kt
@@ -1,13 +1,19 @@
 package me.ibrahimyilmaz.arch_data
 
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 
-inline fun <T> mutableLiveDataOf(): MutableLiveData<T> = MutableLiveData()
+fun <T> mutableLiveDataOf(): MutableLiveData<T> = MutableLiveData()
 
-inline fun <T> publishLiveDataOf(): PublishLiveData<T> = PublishLiveData()
+fun <T> publishLiveDataOf(): PublishLiveData<T> = PublishLiveData()
 
-inline fun <T> replayLiveDataOf(): ReplayLiveData<T> = ReplayLiveData()
+fun <T> replayLiveDataOf(): ReplayLiveData<T> = ReplayLiveData()
 
-
-
+fun <T> LiveData<T>.nonNullObserve(owner: LifecycleOwner, observer: (t: T) -> Unit) {
+    this.observe(owner, Observer {
+        it?.let(observer)
+    })
+}
